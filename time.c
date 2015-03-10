e01e2164f77f4bd79c2f88207bc3fe4452c87e95
ycshu <ycshu@mail.ncku.edu.tw>
2015-03-11 00:04:34 +0800

    change integer operation to double

--- time.c
+++ time.c
@@ -5,11 +5,12 @@ int main()
 {
 	clock_t tic, toc;
 	int i, N, s;
+	double a;
 	N = 1000000000;
 	tic = clock();
 	for(i=0;i<N;++i)
 	{
-		s = 123456789*987654321;
+		a = 12345.6789*98765.4321;
 	}
 	toc = clock();
 	printf("Time: %f\n", 1.0*(toc - tic)/CLOCKS_PER_SEC);
