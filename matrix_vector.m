14a09255869691622fc3b7a7621e663ef09bf037
ycshu <ycshu@mail.ncku.edu.tw>
2015-03-24 20:42:29 +0800

    Compute Matrix * vector

---
+++ matrix_vector.m
@@ -0,0 +1,18 @@
+function matrix_vector(N)
+
+tic;
+for i=1:N
+    x(i) = cos(i*pi/N);
+end
+for i=1:N
+    for j=1:N
+        A(i,j) = 1/(i+j-1);
+    end
+end
+for i=1:N
+    b(i) = 0;
+    for j=1:N
+        b(i) = b(i) + A(i,j)*x(j);
+    end
+end
+toc
