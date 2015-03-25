00fb236e2a5f11c3317598a9988c7a5d3253bfe8
BergAversta <kevin791031123@yahoo.com.tw>
2015-03-25 09:31:47 +0800

    325

---
+++ midexam/325.cpp
@@ -0,0 +1,15 @@
+	#include <stdio.h>
+	
+	int main()
+	{                        // <--註解
+	                         /*..很多注解..*/ 
+	int i,j,k;              //  <--in Z -2^32~2^32
+	double a,b,c;           //  <--in R 
+	double x[10],u[10];           //  x is a vector:x[0],x[1],...x[9]
+	double A[10][10]        //  A is a matrix:
+	}
+	
+	
+	
+	
+	
