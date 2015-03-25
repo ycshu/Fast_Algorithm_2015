e365a919b09649811a636df2c20b8b6582c1a9ce
ycshu <ycshu@mail.ncku.edu.tw>
2015-03-25 10:01:36 +0800

    from baby problem to children problem

--- start.c
+++ start.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <stdio.h>	// 標準的輸入輸出 
+#include <math.h>	// 數學系學到的函數們 
 
 int main()
 {
@@ -12,5 +13,70 @@ int main()
 	double x[10], u[10]; // x is a vector: x[0], x[1], x[2],...,x[9]
 	double A[10][10];    // A is a matrix: A[0][0], A[0][1], ..., A[0][9], ... A[9][9]
 
+	// Moving average, example, 三個平均 
+	/* u[0] = (x[0]+x[1]+x[2])/3
+	   u[1] = (x[1]+x[2]+x[3])/3
+	   u[2] = (x[2]+x[3]+x[4])/3
+	   ... General form
+	   u[i] = (x[i]+x[i+1]+x[i+2])/3
+	   ... Given in program
+	   u[?] = (x[7]+x[8]+x[9])/3 
+	*/
+	// for-loop 
+	// for(initial condition; end condition; action at each step)
+	for(i=0;i<=10;i=i+1)
+	{
+		x[i] = cos(2*i*M_PI/10);
+		//printf("x[%d]=%f\n", i, x[i]);
+	}
+	for(i=0;i<=7;i=i+1)
+	{
+		u[i] = (x[i]+x[i+1]+x[i+2])/3;
+	}
+	/*
+	   3U = [1 1 1 0 0 0 0 ... 0] x[0]
+	        [0 1 1 1 0 0 0 ... 0] x[1]
+	        [ 
+	*/
+	// 以上的是baby problem.....
+	
+	children_problem(20);
+	
+	return 1314520;
+}
+int children_problem(int N)
+{
+	int i, j, k;	     // i, j, k \in Z
+	double a, b, c;      // a, b, c \in R
+	double x[N], u[N]; // x is a vector: x[0], x[1], x[2],...,x[9]
+	double A[N][N];    // A is a matrix: A[0][0], A[0][1], ..., A[0][9], ... A[9][9]
+	//printf("I am a baby-%d!\n",N);
+	// Moving average, example, 三個平均 
+	/* u[0] = (x[0]+x[1]+x[2])/3
+	   u[1] = (x[1]+x[2]+x[3])/3
+	   u[2] = (x[2]+x[3]+x[4])/3
+	   ... General form
+	   u[i] = (x[i]+x[i+1]+x[i+2])/3
+	   ... Given in program
+	   u[?] = (x[7]+x[8]+x[9])/3 
+	*/
+	// for-loop 
+	// for(initial condition; end condition; action at each step)
+	for(i=0;i<N;i=i+1)
+	{
+		x[i] = cos(2*i*M_PI/N);
+		printf("x[%d]=%f\n", i, x[i]);
+	}
+	for(i=0;i<=N-3;i=i+1)
+	{
+		u[i] = (x[i]+x[i+1]+x[i+2])/3;
+	}
+	/*
+	   3U = [1 1 1 0 0 0 0 ... 0] x[0]
+	        [0 1 1 1 0 0 0 ... 0] x[1]
+	        [ 
+	*/
+	// 以上的是children problem.....
 	 
+	return 1314520;
 }
