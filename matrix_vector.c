8b6c62e51721a1879429894d195dd1e8f482a772
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-03-25 11:44:10 +0800

    test pointer

--- matrix_vector.c
+++ matrix_vector.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #define DEBUG 0
 int main()
 {
 	int i, j, N=500;
-	double x[N],b[N],tic,toc;
-	
+	double *x,*b,tic,toc;
+    double **A;
+    x=(double *) malloc( N * sizeof(double));
+    b=(double *) malloc( N * sizeof(double));
+	A=(double **) malloc( N * sizeof(double *));
+    for (i=0; i<N; i++) {
+        A[i]=(double *) malloc( N * sizeof(double));
+    }
+    
 	tic = clock();
 	for(i=0;i<N;++i)
 	{
 		x[i] = cos(i*M_PI/N);
 	}
-	double A[N][N];
+	//double A[N][N];
 	for(i=0;i<N;++i)
 	{
 		for(j=0;j<N;++j)
