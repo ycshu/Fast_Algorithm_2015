60bd8f8406f66c2c782a64a1645863fbe9f69be6
ycshu <ycshu@mail.ncku.edu.tw>
2015-03-25 09:30:59 +0800

    explain the area of variables.

---
+++ start.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	// <--註解
+	/* <--很多注解的開始 
+	   中間可以打很多注解 
+	很多注解的結束--> */
+	// 變數宣告區 
+	int i, j, k;	     // i, j, k \in Z
+	double a, b, c;      // a, b, c \in R
+	double x[10], u[10]; // x is a vector: x[0], x[1], x[2],...,x[9]
+	double A[10][10];    // A is a matrix: A[0][0], A[0][1], ..., A[0][9], ... A[9][9]
+
+	 
+}
