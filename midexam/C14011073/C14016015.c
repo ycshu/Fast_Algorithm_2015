47cc70953bad51d4110ef7f48de1cec844fb9ded
yuronfu <C14011073@mail.ncku.edu.tw>
2015-04-06 21:10:48 +0800

    C14016015 (1)

---
+++ midexam/C14011073/C14016015.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Please write a program:
+
+(1) print 1 3 5 7 9 ... 101
+(2) set a double vector x to be 1 3 5 7 9 ... 101
+(3) compute the sum of this vector
+
+send the file and commit it in your directory :D
+*/
+
+int main()
+{
+    int i;
+    for(i=1;i<=101;i+=2)
+    {
+                       printf("%d ",i);
+    }
+    
+    system("pause");
+}
