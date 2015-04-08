53061cb90d8b32963529095bb9f9f3955c7b711f
C14016112 <byshiue@gmail.com>
2015-04-08 09:51:40 +0800

    I have learned how to discompose a string to many substring.

--- midexam/C14016112/hellow_world.cpp
+++ midexam/C14016112/hellow_world.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
 int main()
 {
-	cout << "Hellow world!" << endl ;
+	 string seperation = " " ; 
+	 string str="Today, I want to show that the complexity time of determinant is n!. But by Gaussian elimination, we can complete it in O(n^3) ";
+	 cout << str << endl ;
+//	 str.erase(0,3);
+//	 cout << str<< endl ;
+	 while( str.length() > 0 )
+	 {
+	 	string substring;
+
+	 	int i = str.find_first_of(seperation,0); 
+	 	substring = str.substr(0,i) ; 
+	 		 	
+	 	cout << substring << endl ;
+//	 	cout << str.length() << endl ;
+	 	str.erase(0,i+1) ; 
+	 	
+	 }
+	 
+	 
 }
